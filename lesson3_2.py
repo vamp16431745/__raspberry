97271a2392f655ac2309c7d08815182ebbbd07e3
Jonathan852 <vamp16431745@gmail.com>
2023-06-12 17:01:24 +0800

    創建按鈕

--- lesson3_2.py
+++ lesson3_2.py
@@ -4,8 +4,13 @@ class Window(tk.Tk):
     def __init__(self):      #def 相當於建構子
         super().__init__()   #self相當於 this  在python預設可以被省略
         self.title('嘿嘿嘿 我被創建了')
-        self.geometry('380x400')
-        self.resizable(False,False)
+        self.resizable(False, False)
+        title_label = tk.Label(self,text="LED控制器",font=('Helvetica', '16') )
+        title_label.pack(pady=25,padx=100)
+
+        led_btn = tk.Button(self,text="LED 開",font=('Helvetica', '16'),padx=20,pady=20)
+        led_btn.pack(pady=(10,50))
+        
 
 if __name__ == "__main__":
     window = Window()
