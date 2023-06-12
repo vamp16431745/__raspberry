467222278d916d273cfeb4033170842f2c8d584a
Jonathan852 <vamp16431745@gmail.com>
2023-06-12 15:56:47 +0800

    建立視窗

---
+++ lessom3_2.py
@@ -0,0 +1,8 @@
+import tkinter
+
+if __name__=="__main__":
+    window=tkinter.Tk()
+    window.title('GUI')
+    window.geometry('380x400')
+    window.resizable(False,False)
+    window.mainloop()
