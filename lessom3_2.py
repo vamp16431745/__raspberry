8a18b1141868b0f52e6a48e8c9fbe1b8eec40cf7
Jonathan852 <vamp16431745@gmail.com>
2023-06-12 16:13:11 +0800

    更新

--- lessom3_2.py
+++ lessom3_2.py
@@ -1,8 +1,12 @@
-import tkinter
+import tkinter as tk #把套件tkinter改暱稱為tk
+
+class Window(tk.Tk):
+    def __init__(self):      #def 相當於建構子
+        super().__init__()   #self相當於 this  在python預設可以被省略
+        self.title('嘿嘿嘿 我被創建了')
+        self.geometry('380X400')
+        self.resizable(False,False)
 
 if __name__=="__main__":
-    window=tkinter.Tk()
-    window.title('GUI')
-    window.geometry('380x400')
-    window.resizable(False,False)
+    window=Window()
     window.mainloop()
