640543585b8b381e2333be1ef293b0564a54f9fa
Jonathan852 <vamp16431745@gmail.com>
2023-06-12 16:27:17 +0800

    456

--- lesson3_2.py
+++ lesson3_2.py
@@ -1,12 +1,12 @@
-import tkinter as tk #把套件tkinter改暱稱為tk
+import tkinter as tk
 
 class Window(tk.Tk):
-    def __init__(self):      #def 相當於建構子
-        super().__init__()   #self相當於 this  在python預設可以被省略
-        self.title("嘿嘿嘿 我被創建了")
-        self.geometry('380X400')
-        self.resizable(False,False)
+    def __init__(self):
+        super().__init__()
+        self.title('這是我的第一個視窗')
+        self.geometry('380x400')
+        self.resizable(False, False)
 
-if __name__=="__main__":
-    window=Window()
+if __name__ == "__main__":
+    window = Window()   
     window.mainloop()
