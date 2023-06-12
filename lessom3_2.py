4e78bbe891dc19ccee9b54f60d5ceb542e721e62
Jonathan852 <vamp16431745@gmail.com>
2023-06-12 16:14:26 +0800

    modify

--- lessom3_2.py
+++ lessom3_2.py
@@ -3,7 +3,7 @@ import tkinter as tk #把套件tkinter改暱稱為tk
 class Window(tk.Tk):
     def __init__(self):      #def 相當於建構子
         super().__init__()   #self相當於 this  在python預設可以被省略
-        self.title('嘿嘿嘿 我被創建了')
+        self.title('first')
         self.geometry('380X400')
         self.resizable(False,False)
 
