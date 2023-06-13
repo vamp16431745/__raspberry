2ad458ac79b5939e44d3aebd8b3228e06e64eb8c
Jonathan852 <vamp16431745@gmail.com>
2023-06-13 10:49:35 +0800

    套用tk裡的ttk去改變按鈕主題

--- lesson3_2.py
+++ lesson3_2.py
@@ -1,15 +1,21 @@
 import tkinter as tk #把套件tkinter改暱稱為tk
+from tkinter import ttk
 
 class Window(tk.Tk):
     def __init__(self):      #def 相當於建構子
         super().__init__()   #self相當於 this  在python預設可以被省略
         self.title('嘿嘿嘿 我被創建了')
         self.resizable(False, False)
-        title_label = tk.Label(self,text="LED控制器",font=('Helvetica', '16') )
+        s = ttk.Style()
+        print(s.theme_names())
+        print(s.theme_use())
+        s.theme_use('clam')
+        title_label = ttk.Label(self,text="LED控制器",font=('Helvetica', '16'))
         title_label.pack(pady=25,padx=100)
 
-        led_btn = tk.Button(self,text="LED 開",font=('Helvetica', '16'),padx=20,pady=20)
+        led_btn = ttk.Button(self,text="LED 開")
         led_btn.pack(pady=(10,50))
+        
         
 
 if __name__ == "__main__":
