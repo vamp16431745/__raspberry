9ce79a620554daf4b8b6b90f6820604498c5fb4b
Jonathan <vamp16431745@gmail.com>
2023-06-19 11:47:50 +0800

    489

--- lesson6_1.py
+++ lesson6_1.py
@@ -5,15 +5,19 @@ from time import sleep
 
 
 if __name__ == "__main__":
-    mcp3008 = zero.MCP3008(channel=7) # 偵測第7個頻
+    mcp3008_ch7 = zero.MCP3008(channel=7) # 偵測第7個頻
+    mcp3008_ch6=zero.MCP3008(channel=6) # 偵測第6個頻
     try:
         while True:
-             value = round(mcp3008.value*100)
+             value = round(mcp3008_ch7.value*100)
+
              print("光敏電阻值: ",value)
              if value > 20:
                  print("光線亮")
              else:
                  print("光線暗")
+
+                 print("LM35: ",mcp3008_ch6.value)
                      
              sleep(1)
     except KeyboardInterrupt:
