fe7b77394b8036eb563f50a56857ee9193a138d2
Jonathan <vamp16431745@gmail.com>
2023-06-27 15:01:28 +0800

    上傳資料

--- lesson6_1.py
+++ lesson6_1.py
@@ -5,26 +5,27 @@ import requests
 
 
 
-
-
 if __name__ == "__main__":
-    mcp3008_ch7 = zero.MCP3008(channel=7) # 偵測第7個頻
-    mcp3008_ch6=zero.MCP3008(channel=6) # 偵測第6個頻
+    mcp3008_ch7 = zero.MCP3008(channel=7)
+    mcp3008_ch6 = zero.MCP3008(channel=6)
     try:
         while True:
-             value = round(mcp3008_ch7.value*100)
+            value = round(mcp3008_ch7.value*100)
+            print("光敏電阻値: ", value)
+            if value > 20:
+                print("光線亮")
+            else:
+                print("光線暗") 
 
-             print("光敏電阻值: ",value)
-             if value > 20:
-                 print("光線亮")
-             else:
-                 print("光線暗")
+            response = requests.get(f'https://vamp16431745-laughing-winner-6pw4jvx5vvg359r7-8000.preview.app.github.dev/raspberry?light={value}&temperature={mcp3008_ch6.value}')
+            
+            if response.ok:
+                print("上傳資料成功")
+                print(response.text)
+            else:
+                print(response.status_code)
 
-                 print("LM35: ",mcp3008_ch6.value*100*3.3)
-
-                 requests.get(f'https://vamp16431745-laughing-winner-6pw4jvx5vvg359r7-8000.preview.app.github.dev/raspberry?light={value}&temperature={mcp3008_ch6.value*100*3.3}')
-                     
-             sleep(5)
+            sleep(5)
     except KeyboardInterrupt:
         GPIO.cleanup()
-        print("程式退函數")    
+        print("程序退函数")
